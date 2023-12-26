cd3cd9b2c20bf84cec246fcdf54ec817e8fa2273
sai sundeep <katakam.sundee7@gmail.com>
2023-12-26 11:41:46 +0530

    ProdctService ase module

---
+++ src/main/java/com/scaler/productservicedecmwfevng/ProductServicedecmwfevngApplication.java
@@ -0,0 +1,14 @@
+package com.scaler.productservicedecmwfevng;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductServicedecmwfevngApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(ProductServicedecmwfevngApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/scaler/productservicedecmwfevng/ProductServicedecmwfevngApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scaler.productservicedecmwfevng;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductServicedecmwfevngApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
